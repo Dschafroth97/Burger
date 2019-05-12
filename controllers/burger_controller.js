35910159ea037cfaeb8bd16bf8047355a5242aa9
Donovan Schafroth <donovanschafroth@gmail.com>
2019-05-12 13:03:59 -0500

    directory paths corrected, img displaying on html, needs styling

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -6,14 +6,14 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
 module.exports = router;
