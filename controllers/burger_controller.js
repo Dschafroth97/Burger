a130d0a6c3f06c838db3bc1fc6cdcfafde3aef13
Donovan Schafroth <donovanschafroth@gmail.com>
2019-05-12 14:55:08 -0500

    Needs to move from available to devoured

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -16,4 +16,25 @@ router.get("/", function (req, res) {
     });
 });
 
+router.post('/burgers', function (req, res) {
+    burger.insertOne([
+        'burger_name'
+    ], [
+            req.body.burger_name
+        ], function (data) {
+            res.redirect('/');
+        });
+});
+
+router.put('/burgers/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (data) {
+        res.redirect('/');
+    });
+});
+
+
 module.exports = router;
