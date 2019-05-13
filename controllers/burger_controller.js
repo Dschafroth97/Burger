0fa3bf7da45e7db76d6764dd83cabf5f106a986b
Donovan Schafroth <donovanschafroth@gmail.com>
2019-05-13 12:06:34 -0500

    functional, ready to deploy

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -16,7 +16,7 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post('/burgers', function (req, res) {
+router.post('/api/burgers', function (req, res) {
     burger.insertOne([
         'burger_name'
     ], [
@@ -26,15 +26,22 @@ router.post('/burgers', function (req, res) {
         });
 });
 
-router.put('/burgers/:id', function (req, res) {
-    var condition = 'id = ' + req.params.id;
-
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
     burger.updateOne({
-        devoured: true
-    }, condition, function (data) {
-        res.redirect('/');
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     });
-});
+  });
 
 
 module.exports = router;
